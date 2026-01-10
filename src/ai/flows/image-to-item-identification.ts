513805ed40116273650e1c2624a6e52225b7906e
MOHIT RATNANI <mohit.ratnani_cs24@gla.ac.in>
2026-01-10 12:37:40 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/con

--- src/ai/flows/image-to-item-identification.ts
+++ src/ai/flows/image-to-item-identification.ts
@@ -18,7 +18,7 @@ const ImageToItemIdentificationInputSchema = z.object({
   photoDataUri: z
     .string()
     .describe(
-      'A photo of the item, as a data URI that must include a MIME type and use Base64 encoding. Expected format: \'data:<mimetype>;base64,<encoded_data>\'.' // Corrected typo here
+      "A photo of the item, as a data URI that must include a MIME type and use Base64 encoding. Expected format: 'data:<mimetype>;base64,<encoded_data>'." // Corrected typo here
     ),
 });
 
