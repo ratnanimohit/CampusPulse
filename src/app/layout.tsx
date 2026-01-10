08f44fe922f739f719d2ba50fb6eed6bda51dee2
MOHIT RATNANI <mohit.ratnani_cs24@gla.ac.in>
2026-01-10 13:52:45 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light" suppressHydrationWarning>
+    <html lang="en" className="light">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body bg-background antialiased" suppressHydrationWarning>
+      <body>
         <FirebaseClientProvider>
           <AppContent>{children}</AppContent>
         </FirebaseClientProvider>
