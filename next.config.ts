8b2ea9a770fb37b6b46252ac78a49f07e7eadb19
MOHIT RATNANI <mohit.ratnani_cs24@gla.ac.in>
2026-01-24 10:13:23 +0000

    Try fixing this error: `Console Error: Google Maps JavaScript API error:

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao',
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: '',
   },
   typescript: {
     ignoreBuildErrors: true,
